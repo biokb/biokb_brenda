0c82998b29760ec52c85f7f820154200bdde6b9b
Christian Ebeling <Christian.Ebeling@scai.fraunhofer.de>
2025-12-18 21:23:37 +0100

    logger info changed

--- src/biokb_brenda/db/manager.py
+++ src/biokb_brenda/db/manager.py
@@ -22,6 +22,7 @@ class DbManager:
         self.data_file_path: str
         connection_str = os.getenv("CONNECTION_STR", DB_DEFAULT_CONNECTION_STR)
         self.engine: Engine = engine if engine else create_engine(connection_str)
+        logger.info(f"Using engine: {self.engine}")
         self.Session = sessionmaker(bind=self.engine)
 
     def download_data_file(self, force: bool = False) -> str:
@@ -49,10 +50,9 @@ class DbManager:
 
                 # Save the content to the file
                 with open(data_file_path, "wb") as file:
+                    logger.info(f"Download data")
                     for chunk in response.iter_content(chunk_size=8192):
                         file.write(chunk)
-
-            logger.info(f"Download complete! File saved as {data_file_path}")
 
         except requests.exceptions.RequestException as e:
             logger.info(f"An error occurred during download: {e}")
@@ -75,10 +75,15 @@ class DbManager:
             raise ValueError("Filename pattern not found in the HTML content.")
 
     def import_data(
-        self, data_file_path: Optional[str] = None, force_download: bool = False
+        self,
+        data_file_path: Optional[str] = None,
+        force_download: bool = False,
+        keep_files: bool = False,
     ):
         """Import data into the database."""
         if data_file_path is None:
             data_file_path = self.download_data_file(force=force_download)
         importer = DbImporter(self.engine)
         importer.import_from_file(data_file_path)
+        if not keep_files:
+            os.remove(data_file_path)
